dd2962525ea8296c2e5f628a48fae5fbe787856d
Trystan11 <trystan.frederick@gmail.com>
2025-09-18 22:37:30 +1000

    added app script file
    for google sheets app script

---
+++ apps_script/googleSheets_script.js
@@ -0,0 +1,31 @@
+function doGet(e) {
+  var ticketId = e.parameter.id;
+  if (!ticketId) {
+    return HtmlService.createHtmlOutput("<h2>No ticket ID provided!</h2>");
+  }
+  
+  var lock = LockService.getDocumentLock(); // Lock sheet for atomic access
+  lock.waitLock(5000); // Wait up to 5 seconds for other processes to finish
+  
+  try {
+    var ss = SpreadsheetApp.getActiveSpreadsheet();
+    var sheet = ss.getSheetByName("Tickets"); // Sheet name
+    var data = sheet.getDataRange().getValues();
+
+    for (var i = 1; i < data.length; i++) {
+      if (data[i][0] == ticketId) { // Ticket ID in column A
+        var status = data[i][3]; // Status in column D
+        if (status == "Checked In") {
+          return HtmlService.createHtmlOutput("<h2>Ticket already used!</h2>");
+        } else {
+          sheet.getRange(i+1, 4).setValue("Checked In");
+          return HtmlService.createHtmlOutput("<h2>Ticket valid! Welcome, " + data[i][1] + "</h2>");
+        }
+      }
+    }
+    return HtmlService.createHtmlOutput("<h2>Ticket not found!</h2>");
+    
+  } finally {
+    lock.releaseLock(); // Always release lock
+  }
+}
